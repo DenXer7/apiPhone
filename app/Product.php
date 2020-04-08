949985f1c004b9c4f0b9d612a99f9178c2b7e27d
Dex <denxerx@gmail.com>
2020-04-07 20:23:08 -0500

    product migration

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'mac',
+        'state',
+        'defect',
+        'price_buy',
+        'price_sale',
+        'price_sale_min',
+        'price_sale_max',
+        'cost',
+        'description'
+    ];
+
+
 }
--- database/migrations/2020_04_07_222642_create_products_table.php
+++ database/migrations/2020_04_07_222642_create_products_table.php
@@ -15,6 +15,19 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigIncrements('id_model_product');
+            $table->bigIncrements('id_branch');
+            
+            $table->string('mac');
+            $table->string('state');
+            $table->string('defect');
+            $table->integer('price_buy');
+            $table->integer('price_sale');
+            $table->string('price_sale_min');
+            $table->string('price_sale_max');
+
+
+
             $table->timestamps();
         });
     }
